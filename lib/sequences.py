c9b2f3f4073c14eff959baa0d57ba01a6d7150ec
Your Name <your.email@example.com>
2025-11-27 14:59:40 +0300

    add this files

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+    fib = []
+    a, b = 0, 1
+    for _ in range(length):
+        fib.append(a)
+        a, b = b, a + b
+    print(fib)
